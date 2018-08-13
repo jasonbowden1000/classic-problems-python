25fd52db19b656d0fa91558c4981950297d325a3
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-13 17:11:31 -0500

    Cleaned up reverse digits spec

--- spec/math/reverse_digits_spec.py
+++ spec/math/reverse_digits_spec.py
@@ -1,15 +1,12 @@
-from mamba import description, context, it
+from mamba import description, it
 from expects import expect, equal
 from problems.math.reverse_digits import reverse_digits
 
-with description('Math') as self:
-    with context('when reversing digits'):
-        with it('should reverse the digits'):
-            expect(reverse_digits(4242)).to(equal(2424))
-        with it('should reverse negative digits'):
-            expect(reverse_digits(-413)).to(equal(-314))
-            pass
-        with it('should not return octals'):
-            expect(reverse_digits(3210)).to(equal(123))
-            pass
+with description('Reversing digits') as self:
+    with it('should reverse positive numbers'):
+        expect(reverse_digits(4242)).to(equal(2424))
+    with it('should reverse negative numbers'):
+        expect(reverse_digits(-413)).to(equal(-314))
+    with it('should not return octals for numbers ending in zero'):
+        expect(reverse_digits(3210)).to(equal(123))
 
