5e46db5100b4bda85494c9f23ef1962b675e9d18
Jason Bowden <jason.bowden1000@gmail.com>
2018-09-03 16:03:38 -0500

    compressed roman_to_arabic

--- problems/string/roman.py
+++ problems/string/roman.py
@@ -1,4 +1,3 @@
-from itertools import tee
 from functools import reduce
 
 roman_dict = {
@@ -6,17 +5,9 @@ roman_dict = {
     'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'M': 1000
 }
 
-def pairwise(iterable):
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
 def roman_to_arabic(str):
-    new_str = list(pairwise(map(lambda x: roman_dict[x], str[::-1])))
-    arabic = new_str[0][0]
-    for pair in new_str:
-        arabic = arabic + pair[1] if pair[0] <= pair[1] else arabic - pair[1]
-    return arabic
+    nums = list(map(lambda x: roman_dict[x], str[::-1]))
+    return reduce(lambda x, t: x + t[1] if t[0] <= t[1] else x - t[1], zip(nums, nums[1:]), nums[0])
 
 def arabic_to_roman():
     pass
